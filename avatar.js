9609911cd4c7c065c33c733e567782e9a8cfcd1a
sjedev <sebastien.jensen@proton.me>
2023-10-05 08:18:43 +0000

    Avatar scaling

--- avatar.js
+++ avatar.js
@@ -2,9 +2,10 @@ class Avatar {
     constructor(initial_x, initial_y){
         this.x = initial_x;
         this.y = initial_y;
+		this.size = tile_size;
     }
 
     show(){
-        image(spritesheet_avatars, this.x, this.y, 64, 64);
+        image(spritesheet_avatars, this.x, this.y, this.size, this.size);
     }
 }
